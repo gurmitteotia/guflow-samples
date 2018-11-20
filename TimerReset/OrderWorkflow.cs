7c0f21aa0943ef176eb30f16d8c28248e1634081
Gurmit Teotia <gurmit.teotia@gmail.com>
2018-11-20 15:23:59 +0000

    Added TimerReset example. Updated to latest nuget package

---
+++ TimerReset/OrderWorkflow.cs
@@ -0,0 +1,36 @@
+﻿// /Copyright (c) Gurmit Teotia. Please see the LICENSE file in the project root folder for license information.
+
+using System;
+using Guflow.Decider;
+
+namespace TimerReset
+{
+    /* In following example when order is place it does not ship it immediately, instead it starts a "GracePeriod" Timer.
+     * You can cancel the order with grace period and you can also reset the grace period on receiving the signal.
+     */
+
+    [WorkflowDescription("1.0", DefaultChildPolicy = ChildPolicy.Terminate,
+        DefaultExecutionStartToCloseTimeoutInSeconds = 10000, DefaultTaskListName = "orderlist",
+        DefaultTaskStartToCloseTimeoutInSeconds = 20)]
+    public class OrderWorkflow : Workflow
+    {
+        public OrderWorkflow()
+        {
+            ScheduleTimer("GracePeriod").FireAfter(TimeSpan.FromMinutes(10))
+                .OnCancel(_ => Jump.ToLambda("OrderCancelled"));
+
+            ScheduleLambda("ShipOrder").AfterTimer("GracePeriod");
+
+            ScheduleLambda("OrderCancelled").AfterTimer("GracePeriod")
+                .When(e => false);
+        }
+
+        [SignalEvent]
+        public WorkflowAction ResetGracePeriod() =>
+            Timer("GracePeriod").IsActive ? Timer("GracePeriod").Reset() : Ignore;
+
+        [SignalEvent]
+        public WorkflowAction CancelOrder()
+            =>Timer("GracePeriod").IsActive ? CancelRequest.For(Timer("GracePeriod")) : Ignore;
+    }
+}
---
+++ TimerReset/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using Amazon;
+using Amazon.Runtime;
+using Amazon.SimpleWorkflow;
+using Guflow;
+using Guflow.Decider;
+
+namespace TimerReset
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync(args).Wait();
+        }
+
+        private static async Task MainAsync(string[] args)
+        {
+            var client = new AmazonSimpleWorkflowClient(new BasicAWSCredentials("access key", "secret key"), RegionEndpoint.EUWest2);
+            var domain = new Domain("GuflowTestDomain", client);
+            await domain.RegisterAsync(10, "test guflow");
+            await domain.RegisterWorkflowAsync<OrderWorkflow>();
+
+           
+
+            using (var workflowHost = domain.Host(new Workflow[] { new OrderWorkflow() }))
+            {
+                workflowHost.OnError(e =>
+                {
+                    Console.WriteLine(e.Exception); return ErrorAction.Continue;
+                });
+                workflowHost.StartExecution();
+                Console.WriteLine("Press a key to terminate");
+                Console.ReadKey();
+            }
+        }
+    }
+}
