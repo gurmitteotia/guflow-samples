929cf6da92963f3a24d083464b145549a1604798
Gurmit Singh <gurmit.singh@cloudhouse.com>
2019-02-08 10:16:37 +0000

    Add lambda functions to support useractivate workflow

---
+++ ServerlessManualApproval/UserActivateFunctions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Amazon.Lambda.Core;
+
+namespace ServerlessManualApproval
+{
+    public class UserActivateFunctions
+    {
+        public string ConfirmEmail(PromotionFunctions.Input input, ILambdaContext context)
+        {
+            Console.WriteLine($"Store passed workflow id {input.Id} in to database to later use it to send signal");
+            Console.WriteLine("Send email to user with links to call back in to your system- another lambda function.");
+            return "Done";
+        }
+
+        public string ActivateUser(string input, ILambdaContext context)
+        {
+            return "Activated";
+        }
+        public class Input
+        {
+            public string Id;
+        }
+    }
+}
